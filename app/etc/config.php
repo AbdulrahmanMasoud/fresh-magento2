6fee3e666f8cbd234f50c27d90a86a4e6bf6e166
Abdulrahman Masoud <abdulrahman.masoud.official@gmail.com>
2024-07-30 12:22:35 +0300

    Install hyva ⚡

--- app/etc/config.php
+++ app/etc/config.php
@@ -327,6 +327,12 @@ return [
         'Magento_Wishlist' => 1,
         'Magento_WishlistAnalytics' => 1,
         'Magento_WishlistGraphQl' => 1,
+        'Hyva_Email' => 1,
+        'Hyva_GraphqlTokens' => 1,
+        'Hyva_GraphqlViewModel' => 1,
+        'Hyva_OrderCancellationWebapi' => 1,
+        'Hyva_Theme' => 1,
+        'Hyva_ThemeFallback' => 1,
         'PayPal_Braintree' => 1,
         'PayPal_BraintreeGraphQl' => 1,
         'Temando_ShippingRemover' => 1
